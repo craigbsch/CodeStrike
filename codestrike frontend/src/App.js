b8f1009c2f43f532e01da98aa5d5c96141cd35b2
Haley-Ogier <hogier@umass.edu>
2024-11-11 03:29:15 -0500

    Sign Up Page Complete

--- codestrike frontend/src/App.js
+++ codestrike frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Signup from './Signup.js';
+import logo from './logo.png';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="signup-container">
+          <Signup />
+        </div>
+      </div>
     </div>
   );
 }
