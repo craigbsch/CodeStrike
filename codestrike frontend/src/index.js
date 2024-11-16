1a0c0154d4a8c69bf1e0d8a52b2fbd4097ec6ea2
Riddhimaan-Senapati <rsenapati@umass.edu>
2024-11-15 19:44:02 -0500

    moved all pages to src/pages

--- codestrike frontend/src/index.js
+++ codestrike frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Homepage from './homepage';
+import Homepage from './pages/homepage';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
