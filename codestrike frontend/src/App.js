f352268c13e55e8d6eac6a57a5fa2f8228c503d3
fferdous <fferdous@umass.edu>
2024-11-18 16:14:24 -0500

    AccountPage

--- codestrike frontend/src/App.js
+++ codestrike frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AccountPage from './AccountPage'; 
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="accountpage-container">
+          <AccountPage />
+        </div>
+      </div>
     </div>
   );
 }
