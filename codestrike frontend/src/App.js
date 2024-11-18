4ec6152ff709914d3b3993e655ed0d0a3e01615d
fferdous <fferdous@umass.edu>
2024-11-18 16:48:29 -0500

    Account page changes

--- codestrike frontend/src/App.js
+++ codestrike frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import AccountPage from './AccountPage'; 
-import logo from './logo.png';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <img src={logo} className="App-logo" alt="logo" />
         <div className="accountpage-container">
           <AccountPage />
         </div>
