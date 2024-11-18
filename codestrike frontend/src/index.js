077f9673d2721f5508b09c836d71c23eb5d186d4
Haley-Ogier <hogier@umass.edu>
2024-11-18 17:16:57 -0500

    Cleanup of Structure

--- codestrike frontend/src/index.js
+++ codestrike frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Homepage from './pages/homepage';
 import { BrowserRouter } from 'react-router-dom';
+import Signup from './pages/Signup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
