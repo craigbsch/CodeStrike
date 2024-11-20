307765cd7896c936b714dc8c97320130d82410c7
Haley-Ogier <hogier@umass.edu>
2024-11-20 03:18:37 -0500

    cleaned

--- codestrike frontend/src/index.js
+++ codestrike frontend/src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Homepage from './pages/homepage';
 import { BrowserRouter } from 'react-router-dom';
 import Signup from './pages/Signup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Homepage />
+    <Signup />
   </BrowserRouter>
 );
 
