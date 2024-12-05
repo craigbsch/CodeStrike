f5676a6b94b6cf9024ad041de02bda6eb84a203e
Haley-Ogier <hogier@umass.edu>
2024-12-04 21:58:19 -0500

    added wrappers

--- codestrike frontend/src/pages/VictoryPage.js
+++ codestrike frontend/src/pages/VictoryPage.js
@@ -4,12 +4,14 @@ import { useNavigate } from 'react-router-dom';
 
 const VictoryPage = () => {
   return (
-    <div className="victory-page">
-      <div className="victory-container">
-        <h1 className="victory-title">Victory!</h1>
-        <img src="/crown.png" alt="Crown" className="victory-crown" />
-        <p className="victory-username">Your Username</p>
-        <button className="victory-button">Continue</button>
+    <div className="victory-page-wrapper">
+      <div className="victory-page">
+        <div className="victory-container">
+          <h1 className="victory-title">Victory!</h1>
+          <img src="/crown.png" alt="Crown" className="victory-crown" />
+          <p className="victory-username">Your Username</p>
+          <button className="victory-button">Continue</button>
+        </div>
       </div>
     </div>
   );
